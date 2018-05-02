5c244a8ea73f3648378e32a52283b7793e5d3963
Respire <firlstoner@gmail.com>
2018-05-02 16:06:21 +0800

    base and first command
    hmm

---
+++ base.js
@@ -0,0 +1,11 @@
+// guild role color: // .setColor(`${message.guild.me.displayHexColor!=='#000000' ? message.guild.me.displayHexColor : config.eColor}`)
+// message author role color: //.setColor(`${message.member.displayHexColor!=='#000000' ? message.member.displayHexColor : config.eColor}`)
+// ​
+const Discord = require("discord.js");
+
+exports.run = async (client,message,args,config,dir) => {
+  if(message.author.bot) return;
+  if(message.channel.type !== "text") return;
+  const guildColor = message.guild.me.displayHexColor!=='#000000' ? message.guild.me.displayHexColor : config.eColor
+  const userColor = message.member.displayHexColor!=='#000000' ? message.member.displayHexColor : config.eColor
+}
