350f13ddff540ce1b43d5ffd147de53621648937
Respire <firlstoner@gmail.com>
2018-05-03 16:42:33 +0800

    Help Command

--- commands/help.js
+++ commands/help.js
@@ -8,5 +8,9 @@ exports.run = async (client,message,args,config,dir) => {
   if(message.channel.type !== "text") return;
   const guildColor = message.guild.me.displayHexColor!=='#000000' ? message.guild.me.displayHexColor : config.eColor
   const userColor = message.member.displayHexColor!=='#000000' ? message.member.displayHexColor : config.eColor
-  message.channel.send("I don't have help command!")
+  const embed = new Discord.RichEmbed()
+  .setColor(guildColor)
+  .setAuthor(`${message.author.tag} Requested Help!`,message.author.avatarURL)
+  .setDescription(`\`y.online\` ~Send LegameMC Server Status
+\`y.ping\` ~ Ping Yamashiro!`)
 }
