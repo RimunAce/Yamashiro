a7e59aaaeb1a47cb493b20e0ecc868871f01d5bd
Respire <firlstoner@gmail.com>
2018-05-03 17:10:31 +0800

    s

---
+++ README.md
@@ -0,0 +1,162 @@
+[![2b1](http://i.imgur.com/LyhaP3h.jpg)](http://arc.moe)
+[![2b2](http://i.imgur.com/W87etD2.jpg)](https://discordapp.com/oauth2/authorize?permissions=8&scope=bot&client_id=291311819354800150)
+[![2b3](http://i.imgur.com/1WetvDx.jpg)](#installation)
+
+[![Discord](https://discordapp.com/api/guilds/290982567564279809/embed.png)](#)
+[![AppVeyor](https://img.shields.io/appveyor/ci/Fshy/FshyBot.svg?style=flat-square)](https://ci.appveyor.com/project/Fshy/fshybot)
+[![David](https://img.shields.io/david/Fshy/FshyBot.svg?style=flat-square)](https://david-dm.org/Fshy/FshyBot)
+[![David](https://img.shields.io/david/dev/Fshy/FshyBot.svg?style=flat-square)](https://david-dm.org/Fshy/FshyBot?type=dev)
+---
+<h2><p align="center">Commands</p></h2>
+
+<h5>General</h5>
+<pre>
+!help                     - Displays all available commands
+!ping                     - Displays response time to server
+!diag                     - Displays bot diagnostics and channel permissions
+!say &lt;text&gt;               - Repeat the given text in a neat RichEmbed message
+!logs &lt;number&gt;            - Posts the most recent VoiceChannel logs (User Join/Move/Disconnect)
+!invite                   - Generates a link to invite 2B to your server
+</pre>
+
+<h5>Admin</h5>
+<pre>
+Requires user to have a role titled "Admin"
+!setprefix &lt;newprefix&gt;    - Sets the prefix that the bot listens to
+</pre>
+
+<h5>Music</h5>
+
+<pre>
+When requesting a song, reaction controls are available under the bot response message.
+[▶] Play/Pause [⬛] Stop [🔁] Replay Song [X] Leave channel
+
+!play &lt;search/link&gt;       - Searches and plays a given song
+!playlist &lt;link&gt; &lt;shuffle&gt;- Queues a given YouTube playlist; Pass 'shuffle' as 2nd param to randomize
+!stop                     - Stops the current song
+!pause                    - Pauses playback of the current song
+!resume                   - Resumes playback of the current song
+!clear                    - Clears the current playlist queue
+!join                     - Joins the user's VoiceChannel
+!leave                    - Stops any playback and leaves the channel
+!stream &lt;url&gt;             - Plays a given audio stream, or file from direct URL
+!radio                    - Displays some available preprogrammed radio streams
+!np                       - Displays Now Playing info for radio streams
+</pre>
+
+<h5>Games</h5>
+
+<pre>
+!pubg &lt;username&gt; &lt;region&gt; - PLAYERUNKNOWN'S BATTLEGROUNDS stats lookup (Regions:na/sa/eu/as/sea/oc)
+!hs &lt;card name&gt;           - Performs a search and returns basic hearthstone card information 
+</pre>
+
+<h5>Anime/NSFW</h5>
+
+<pre>
+!smug                     - Posts a random smug reaction image
+!2B &lt;nsfw&gt;                - Uploads a random 2B image, or a NSFW version if supplied as a parameter
+
+!tags &lt;search term&gt;       - Searches Danbooru for possible related search tags
+!sfw &lt;tag&gt;                - Uploads a random SFW image from Danbooru, for up to two search tags
+!nsfw &lt;tag&gt;               - Uploads a random NSFW image from Danbooru, for up to two search tags
+
+Multiple tags are separated by a / symbol, Example: !sfw kancolle/thighhighs
+</pre>
+
+<h5>Web APIs</h5>
+
+<pre>
+!insta &lt;username&gt;         - Uploads the most recent instagram post from a given user
+!crypto &lt;coin&gt; &lt;amount&gt;   - Displays current cryptocurrency price or calculated value (optional)
+!r &lt;subreddit&gt;            - Uploads a random image from a given subreddit
+</pre>
+
+<h5>Misc</h5>
+
+<pre>
+!calc &lt;expression&gt;        - Evaluates a given expression
+!roll &lt;sides&gt; &lt;num&gt;       - Rolls an n-sided die, m times and displays the result
+</pre>
+
+<h5>Chatbot</h5>
+
+<pre>
+2B answers her callsign in response to the user
+Examples:
+  Fshy: 2B How are you?
+  2B:   Great! how about you?
+
+  Fshy: 2B What's the time?
+  2B:   It is 1:00 A.M.
+</pre>
+
+<hr>
+<h2><p align="center">Installation</p></h2>
+<h3>Prerequisites</h3>
+
+* [Node.js](https://nodejs.org/en/download/) (Version 8.0.0 or newer)
+* [python 2.7](https://www.python.org/download/releases/2.7/)
+* [ffmpeg](https://www.ffmpeg.org/download.html)
+
+<h5>Windows:</h5>
+
+```shell
+#Install Microsoft Build Tools
+npm install --global --production windows-build-tools
+```
+
+<h5>Ubuntu/Linux:</h5>
+
+```shell
+#Install Node/npm
+$ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+$ sudo apt-get install -y nodejs
+
+#Install python2.7 and ffmpeg
+$ sudo apt-get install -y python2.7 ffmpeg
+
+#Install required build tools (make, gcc)
+$ sudo apt-get install -y build-essential
+
+#Configure python for node-gyp
+$ npm config set python python2.7
+```
+
+<hr>
+
+<h3>Build</h3>
+
+```shell
+$ git clone https://github.com/Fshy/FshyBot
+$ cd FshyBot
+$ npm install
+$ chmod 0775 app.js
+```
+
+Rename `config_example.json` to `config.json` and add your user token/API keys to suit.
+
+<hr>
+
+<h3>Running</h3>
+
+With a working config file in place, we're ready to start the script from the terminal/command prompt.
+
+```shell
+#Launch bot
+$ node app.js
+```
+
+It's recommended however, to run scripts using a process manager such as [PM2](https://github.com/Unitech/pm2). This ensures your bot automatically restarts in case of a crash. This also allows us to use the !update command to fetch new builds.
+
+```shell
+#Install pm2
+$ npm install pm2 -g
+
+#Launch using pm2
+$ pm2 start app.js --name "2B"
+
+# Configure pm2 to launch on startup
+$ pm2 startup
+$ pm2 save
+```
---
+++ app.js
@@ -0,0 +1,297 @@
+/*jshint esversion: 6 */
+
+const request   = require('request');
+const snekfetch = require('snekfetch');
+const Discord   = require('discord.js');
+const _         = require('lodash');
+const fs        = require('fs');
+const ytdl      = require('ytdl-core');
+const math      = require('mathjs');
+const winston   = require('winston');
+const config    = require('./config');
+const version   = require('./package').version;
+const commands  = require('./commands');
+const lib       = require('./lib');
+
+winston.configure({
+  transports: [
+    new (winston.transports.File)({
+      filename: 'winston.log',
+      timestamp: function () {
+        return +new Date()
+      }
+    })
+  ]
+});
+
+global.guildsMap = new Map();
+global.client    = new Discord.Client();
+var timer;
+
+client.login(process.env.token);
+
+client.on('ready', () => {
+  if (!fs.existsSync('guildRecords.json')) {
+    fs.writeFileSync('guildRecords.json', JSON.stringify({}, null, '\t'), {});
+      var clientGuilds = client.guilds.keyArray();//Cached Guilds
+      for (var i = 0; i < clientGuilds.length; i++) {
+        if (!guildsMap.has(clientGuilds[i])) {
+          guildsMap.set(clientGuilds[i],{prefix:config.prefix});
+        }
+      }
+      lib.writeMapToFile(guildsMap);//Write to file
+  }else {
+    var fileMap = new Map(lib.readFileToMap());//Read from file
+    guildsMap = lib.readFileToMap();
+    var clientGuilds = client.guilds.keyArray();//Cached Guilds
+    for (var i = 0; i < clientGuilds.length; i++) {
+      if (!guildsMap.has(clientGuilds[i])) {
+        guildsMap.set(clientGuilds[i],{prefix:config.prefix});
+      }
+    }
+    if (!_.isEqual(lib.map_to_object(guildsMap),lib.map_to_object(fileMap))) lib.writeMapToFile(guildsMap);//Write to file if not matching - i.e. new guild prefs
+  }
+  console.log(`\n\x1b[32m\x1b[1m// ${config.name} Online and listening for input\x1b[0m`);
+  lib.postDiscordBots();
+  // Alternate setGame
+  var i = 0;
+  timer = client.setInterval(function () {
+    var gamePresence = [
+      `NieR: Automata™`,
+      `from arc.moe`,
+      `Commands: !help`,
+      `Support via GitHub`,
+      `on ${client.guilds.size} Guilds for ${client.users.size} Users`
+    ];
+    client.user.setPresence({ game: { name: gamePresence[i%gamePresence.length], type: 0 } });
+    i++;
+  },7500);
+  // Log Current Stats
+  client.setInterval(function () {
+    var d = new Date(Date.now());
+    console.log(`\n\x1b[32m[${d.getDate()}/${(d.getMonth()+1)}/${d.getFullYear()} | ${d.toLocaleTimeString()}]\x1b[0m Mem: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB | Users: ${client.users.size} | Guilds: ${client.guilds.size}`);
+  },300000);
+});
+
+client.on('guildCreate', (guild)=>{
+  // if (!guildsMap.has(guild.id)) {//Uncomment this to retain preferences after guild removal
+    guildsMap.set(guild.id,{prefix:config.prefix});
+    lib.writeMapToFile(guildsMap);//Write to file
+  // }
+  lib.postDiscordBots();
+// CHANGED defaultChannel deprecated in discord-js v12
+//   if (guild.defaultChannel.permissionsFor(client.user).has('SEND_MESSAGES')) {//Has write permissions
+//   guild.defaultChannel.send({embed:new Discord.RichEmbed()
+//     .setTitle(`// ${client.user.username} is now serving ${guild.name}`)
+//     .setDescription(`
+// Thanks for adding me to your server!
+// Please have a look at my command list using **!help**
+// or for more detailed information at [GitHub](https://github.com/Fshy/FshyBot) | [arc.moe](http://arc.moe)
+//
+// Currently running v${version} on a ${process.platform}-${process.arch} platform`)
+//     .setThumbnail(client.user.displayAvatarURL)
+//     .setColor(`${guild.me.displayHexColor!=='#000000' ? guild.me.displayHexColor : config.hexColour}`)});
+//   }
+});
+
+client.on('guildDelete', (guild)=>{
+  lib.postDiscordBots();
+});
+
+// CHANGED defaultChannel deprecated in discord-js v12
+// client.on('guildMemberAdd', (member) => {
+//   if (member.guild.defaultChannel.permissionsFor(client.user).has('SEND_MESSAGES')) {//Has write permissions
+//     member.guild.defaultChannel.send({embed:new Discord.RichEmbed()
+//       .setDescription(`${member.nickname ? `${member.displayName} (${member.user.username})` : member.user.username} has joined the server.\nPlease welcome them to ${member.guild.name}`)
+//       .setThumbnail(member.user.displayAvatarURL)
+//       .setColor(`${member.guild.me.displayHexColor!=='#000000' ? member.guild.me.displayHexColor : config.hexColour}`)});
+//   }
+// });
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  if (oldMember.voiceChannel) {//Was in a voiceChannel
+    if (newMember.voiceChannel !== oldMember.voiceChannel) {//If state changed, but channel remains the same i.e. Mute, Deafen
+      if (newMember.voiceChannel) {//If moved to a new voiceChannel
+        winston.log('info', `${newMember.nickname ? `${newMember.displayName} (${newMember.user.username})` : newMember.user.username} moved from ${oldMember.voiceChannel.name} to ${newMember.voiceChannel.name}`, {guildID: oldMember.guild.id, type: 'voice'});
+      }else {
+        winston.log('info', `${newMember.nickname ? `${newMember.displayName} (${newMember.user.username})` : newMember.user.username} disconnected from ${oldMember.voiceChannel.name}`, {guildID: oldMember.guild.id, type: 'voice'});
+      }
+    }
+  }else {//Was not in a voiceChannel
+    if (newMember.voiceChannel !== oldMember.voiceChannel) {//If connected to a new voiceChannel
+      winston.log('info', `${newMember.nickname ? `${newMember.displayName} (${newMember.user.username})` : newMember.user.username} connected to ${newMember.voiceChannel.name}`, {guildID: newMember.guild.id, type: 'voice'});
+    }
+  }
+});
+
+client.on('messageReactionAdd', (messageReaction,user)=>{
+  if (user.bot) return;
+  if (messageReaction.message.author.id!==client.user.id) return;
+  switch (messageReaction.emoji.identifier) {
+    case '%E2%8F%AF'://PlayPause
+      let vconnec = client.voiceConnections.get(messageReaction.message.guild.id);
+      if (vconnec) {
+        let dispatch = vconnec.player.dispatcher;
+        if (dispatch){
+          if (dispatch.speaking)
+            dispatch.pause();
+          else
+            dispatch.resume();
+        }
+      }
+      break;
+    case '%E2%8F%B9'://Stop
+      lib.clearQueue(guildsMap,client,messageReaction.message);
+      commands.stop(guildsMap,client,messageReaction.message);
+      break;
+    case '%E2%8F%AD'://Next Track
+      commands.stop(guildsMap,client,messageReaction.message);
+      break;
+    case '%F0%9F%94%81'://Repeat
+      commands.repeat(ytdl,winston,guildsMap,client,user,messageReaction.message);
+      break;
+    case '%E2%9D%8C'://Leave
+      commands.leave(guildsMap,client,messageReaction.message);
+      break;
+  }
+});
+
+client.on('messageReactionRemove', (messageReaction,user)=>{
+  if (user.bot) return;
+  if (messageReaction.message.author.id!==client.user.id) return;
+  switch (messageReaction.emoji.identifier) {
+    case '%E2%8F%AF'://PlayPause
+      let vconnec = client.voiceConnections.get(messageReaction.message.guild.id);
+      if (vconnec) {
+        let dispatch = vconnec.player.dispatcher;
+        if (dispatch){
+          if (dispatch.speaking)
+            dispatch.pause();
+          else
+            dispatch.resume();
+        }
+      }
+      break;
+    case '%E2%8F%B9'://Stop
+      lib.clearQueue(guildsMap,client,messageReaction.message);
+      commands.stop(guildsMap,client,messageReaction.message);
+      break;
+    case '%E2%8F%AD'://Next Track
+      commands.stop(guildsMap,client,messageReaction.message);
+      break;
+    case '%F0%9F%94%81'://Repeat
+      commands.repeat(ytdl,winston,guildsMap,client,user,messageReaction.message);
+      break;
+    case '%E2%9D%8C'://Leave
+      commands.leave(guildsMap,client,messageReaction.message);
+      break;
+  }
+});
+
+client.on('message', (message)=>{
+  if(message.author.bot) return;
+
+  // Receive DM
+  if (message.channel.type===`dm`)
+    return commands.chatbot(client,message.content.split(),message);
+
+  // Custom Prefixes
+  let guildPrefix = config.prefix;
+  if (guildsMap.has(message.guild.id)) guildPrefix = guildsMap.get(message.guild.id).prefix;
+  if(!message.content.startsWith(guildPrefix) && !message.content.startsWith(`2B`) && !message.content.startsWith(`2b`)) return;
+
+  // Command Parsing
+  console.log(`\x1b[36m[${message.guild}] \x1b[1m${message.author.username}: \x1b[0m${message.content}`);
+  let command = message.content.split(/\s+/g)[0].slice(guildPrefix.length);
+  let args    = message.content.split(/\s+/g).slice(1);
+
+  switch (command.toLowerCase()) {
+
+    // TO-DO List
+    // TODO Modularize Commands
+    // Notes: These commands may not disabled themselves | Require MANAGE_GUILD Perms
+    // case 'disable':     return commands.disableCommand(guildDB,guildsMap,guildPrefix,args,message);
+    // case 'enable':      return commands.enableCommand(guildDB,guildsMap,guildPrefix,args,message);
+    // TODO Apply a SEND_MESSAGES check before attempting output
+    // TODO Adjust Embed Colours based on message type (success, error, warning, info)
+    // TODO Use createReactionCollector() to generate timed responses/polls
+    // TODO Check that [current version < stable version] before allowing !update | Display changelog/commit messages
+    // TODO Winston logs implement users @mentions
+    // TODO Start logging messages per user
+    // TODO Implement controls for stream
+    // TODO Localization on a per-guild basis
+    // TODO Perform check if guildRecords.json exists on ready
+    // TODO Trim paramters using guildPrefix => guildsMap.get(message.guild.id).prefix
+    // TODO Process guildsMap as a global variable
+    // TODO Catch errors from promises
+    // TODO Implement ReactionCollector for upvote/downvote system with images & moderation
+    // TODO Instagram lookup by hashtags or location
+
+    // General
+    case 'help':        return commands.help(guildPrefix,message);
+    case 'ping':        return commands.ping(client,message);
+    case 'diag':        return commands.stats(version,client,message);
+    case 'version':     return commands.ver(version,guildPrefix,message);
+    case 'invite':      return commands.invite(client,message);
+    case 'say':         return commands.say(guildPrefix,message);
+    case 'logs':        return commands.logs(args,message);
+
+    // Owner Commands
+    case 'update':      return commands.update(message);
+    // case 'broadcast':   return commands.broadcast(client,guildPrefix,args,message); // CHANGED defaultChannel deprecated in discord-js v12
+
+    // Admin Commands
+    case 'setname':     return commands.setName(client,args,message);
+    case 'setgame':     return commands.setGame(timer,client,args,message);
+    case 'setavatar':   return commands.setAvatar(client,args,message);
+    case 'setstatus':   return commands.setStatus(client,guildPrefix,args,message);
+    case '2bsetprefix':
+    case 'setprefix':   return commands.setprefix(guildsMap,guildPrefix,args,message);
+
+    // Music
+    // case 'controls':    return commands.controls(guildsMap,client,message);
+    case 'join':        return commands.join(message);
+    case 'leave':       return commands.leave(guildsMap,client,message);
+    case 'play':        return commands.play(ytdl,winston,guildsMap,client,args,message);
+    case 'playlist':    return commands.playlist(ytdl,guildsMap,client,args,message);
+    case 'clear':       return commands.clearQueue(guildsMap,client,message);
+    case 'stop':        return commands.stop(guildsMap,client,message);
+    case 'pause':       return commands.pause(client,message);
+    case 'resume':      return commands.resume(client,message);
+    case 'stream':      return commands.stream(guildsMap,client,args,message);
+    case 'radio':       return commands.radio(guildsMap,args,message);
+    case 'np':
+    case 'nowplaying':  return commands.nowPlaying(guildsMap,message);
+
+    // Games
+    case 'pubg':        return commands.pubg(args,message);
+    case 'hs':
+    case 'hearthstone': return commands.hearthstone(args,message);
+
+    // Web APIs
+    case 'r':           return commands.rslash(guildPrefix,message,args);
+    case 'crypto':      return commands.coin(args,message);
+    case 'insta':       return commands.insta(args,message);
+    case 'time':        return commands.timezone(args,message);
+
+    // Anime/NSFW
+    case 'anime':       return commands.anime(args,message);
+    case 'sfw':         return commands.danbooru(guildPrefix,args,`s`,message);
+    case 'nsfw':
+    case 'lewd':        return commands.danbooru(guildPrefix,args,`e`,message);
+    case 'tags':        return commands.danbooruTags(args,message);
+    case '2b':          return commands.img2B(guildPrefix,args,message);
+    case 'smug':        return commands.smug(message);
+
+    // Misc
+    case 'calc':        return commands.calc(math,args,message);
+    case 'roll':        return commands.roll(args,message);
+
+  }
+
+  // Chatbot
+  if (message.content.startsWith(`2B`) || message.content.startsWith(`2b`)){
+    return commands.chatbot(client,message.content.split(/\s+/g).slice(1),message);
+  }
+
+});
